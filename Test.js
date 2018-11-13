d221dbb6a177db299f8d78b007743acecdbbac89
moransaka <moransaka15@gmail.com>
2018-11-13 21:43:31 +0200

    add

--- Test.js
+++ Test.js
@@ -1,7 +1,39 @@
-let array = ['q','w','e','r','t','y','u','i','o','p'];
-let n = 4, m = 6;
-for (let i = 0; i <= (n-1); i++){
-    array.push(array[i]);
+function twins (x,y,k,m){
+    var n=[], s=[], t=0, j=0;
+    k=+prompt("Введите длину массива x:");
+    m=+prompt("Введите длину массива y: ");
+    for (i=0; i<k; i++){
+        x = +prompt("Введите число x:");
+        n.push(x);
+    }
+
+    for (i=0; i<m; i++){
+        y =+prompt("Введите число y:");
+        s.push(y);
+    }
+
+    if (s.length > n.length){
+        for(i=0; i<s.length; i++){
+            if(n[j] == s[i]) t++;
+                
+            if(i==s.length){
+                j=j+1;
+                i=0;
+            }
+        }
+    }
+
+    if (s.length < n.length){
+        for(i=0; i<n.length; i++){
+            if(n[i] == s[j]){
+            t++;
+                if(i==n.length){
+                    j=j+1;
+                    i=0;
+                }
+            }
+        }
+    }
+    return alert(s + " " + n + " " + t);
 }
-array.splice(0,n);
-console.log(array);
+    twins();
